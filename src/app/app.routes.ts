31a25f474ea49fad7c580debd47cff42495d3c51
Manuel Aguilera <102045443+aguileraDev@users.noreply.github.com>
2025-01-25 17:35:18 -0400

    docs: update config and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { SidebarComponent } from '@components/dashboard/components/sidebar/sidebar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () => import('@components/dashboard/dashboard.component').then(module => module.DashboardComponent),
+    children: [
+      {
+        path: '',
+        component: SidebarComponent,
+        outlet: 'secondary',
+      },
+      {
+        path: 'customer',
+        loadComponent: () => import('@components/customer/customer.component').then(module => module.CustomerComponent)
+      },
+
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+      },
+    ],
+  },
+
+  { path: '**',
+    redirectTo: 'dashboard'
+  },
+];
